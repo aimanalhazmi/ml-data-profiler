8c00b30355e2220cab1b4dfa409417d4c4e10d95
aimanalhazmi <aimanalhazmi@gmail.com>
2025-07-03 16:02:00 +0200

    Add save_file Var

--- src/ingestion/loader.py
+++ src/ingestion/loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 from src.ingestion.ingestorFactory import IngestorFactory
 
 
-def load_dataset(url: str) -> pd.DataFrame:
-    ingestor_factory = IngestorFactory(url, 1)
+def load_dataset(url: str, save_file=False) -> pd.DataFrame:
+    ingestor_factory = IngestorFactory(url, 1, save_file)
     ingestor = ingestor_factory.create()
     return ingestor.load_data()
