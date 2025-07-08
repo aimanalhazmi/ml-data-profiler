03dea35b6b8b73dcb034468e8f1d9535f9d60bce
luloduarte <duarte.castineira@campus.tu-berlin.de>
2025-07-08 19:25:07 +0200

    multiclass label outlier calculation

--- src/quality/compare.py
+++ src/quality/compare.py
@@ -14,7 +14,7 @@ from model.train import train_model
 def compare_outlier_removals(X_train, X_test, y_train, y_test, num_cols, model, alpha = 0.01, sigma_multiplier = 1.0, model_type='logistic'):
     # train baseline on original data and get f1 score
     baseline = train_model(X_train.values, y_train.values, model_type)
-    f1_orig = f1_score(y_test, baseline.predict(X_test.values))
+    f1_orig = f1_score(y_test, baseline.predict(X_test.values), average='macro')
 
     # train without statistical outliers and get f1
     stat_mask_full = mahalanobis_outliers(X_train, X_test, num_cols, alpha=alpha)
@@ -22,7 +22,7 @@ def compare_outlier_removals(X_train, X_test, y_train, y_test, num_cols, model, 
     X_train_stat = X_train.loc[~stat_train_mask]
     y_train_stat = y_train.loc[~stat_train_mask]
     stat_clf = train_model(X_train_stat.values, y_train_stat.values, model_type)
-    f1_statistic = f1_score(y_test, stat_clf.predict(X_test.values))
+    f1_statistic = f1_score(y_test, stat_clf.predict(X_test.values), average='macro')
 
     # train without influence outliers and get f1
     infl_mask_full = influence_outliers(X_train, X_test, y_train, y_test, model, frac = 0.001, random_state= 912, sigma_multiplier = sigma_multiplier)
@@ -30,7 +30,7 @@ def compare_outlier_removals(X_train, X_test, y_train, y_test, num_cols, model, 
     X_train_infl = X_train.loc[~infl_train_mask]
     y_train_infl = y_train.loc[~infl_train_mask]
     infl_clf = train_model(X_train_infl.values, y_train_infl.values, model_type)
-    f1_influence = f1_score(y_test, infl_clf.predict(X_test.values))
+    f1_influence = f1_score(y_test, infl_clf.predict(X_test.values), average='macro')
 
     return pd.DataFrame([{
         'f1_orig': f1_orig,
